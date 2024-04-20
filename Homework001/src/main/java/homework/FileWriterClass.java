5fdd2ed9885261ec133bf36b4001bc950793b404
Ali YILDIRIM <aliyildirim850@gmail.com>
2024-04-20 21:23:31 +0300

    HomeworkAboutPostgresql

---
+++ Homework001/src/main/java/homework/FileWriterClass.java
@@ -0,0 +1,41 @@
+package homework;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.*;
+
+public class FileWriterClass {
+    public static void writeToFile() {
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/ali_db",
+                    "postgres",
+                    "123");
+
+
+            String sql = "SELECT * FROM information";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+
+            FileWriter filewriter = new FileWriter("personalInformation.txt");
+
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String surname = resultSet.getString("surname");
+                int age = resultSet.getInt("age");
+                String phoneNumber = resultSet.getString("phone_number");
+                String gender = resultSet.getString("gender");
+
+                String line = String.format("Name: %s, Surname: %s, Age: %d, Phone number: %s, Gender: %s%n", name, surname, age, phoneNumber, gender);
+                filewriter.write(line);
+            }
+
+            filewriter.close();
+            connection.close();
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
